01ff99ef42ef860cd998e77eee76c997f6395dcf
TungHoi <1207726295@qq.com>
2022-11-02 14:46:03 +0800

    1

---
+++ Coordinator/Coordinator.ino
@@ -0,0 +1,57 @@
+#include<Servo.h>
+Servo S;
+
+void setup() {
+  Serial.begin(9600);
+  S.attach(13);
+  pinMode(10, OUTPUT);
+  digitalWrite(10, HIGH);
+  delay(3000);
+}
+
+void loop() {
+  
+  // step 1
+  Serial.flush();
+  Serial.write(0x90);
+  Serial.flush();
+  Serial.println(90);
+  delay(10000);
+
+  // step 1.5
+  Serial.flush();
+  Serial.write(0x91);
+  Serial.flush();
+  delay(13000);
+
+  // step 2
+  Serial.flush();
+  Serial.write(0x92);
+  Serial.flush();
+  delay(23000);
+
+  // step 2.5
+  Serial.flush();
+  Serial.write(0x93);
+  Serial.flush();
+  delay(8000);
+
+  // step3
+  Serial.flush();
+  Serial.write(0x94);
+  Serial.flush();
+  delay(8000);
+
+  // step3.5
+  Serial.flush();
+  Serial.write(0x95);
+  Serial.flush();
+  delay(30000);
+
+  // step 4
+  Serial.flush();
+  Serial.write(0x96);
+  Serial.flush();
+  delay(4000);
+
+}
