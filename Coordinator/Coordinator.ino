da50a07571e7aef7308c69cd90b0d571efeb6cc1
TungHoi <1207726295@qq.com>
2022-11-03 23:04:03 +0800

    11

--- Coordinator/Coordinator.ino
+++ Coordinator/Coordinator.ino
@@ -11,47 +11,47 @@ void setup() {
 
 void loop() {
   
-  // step 1
+    // step 1
   Serial.flush();
   Serial.write(0x90);
   Serial.flush();
   Serial.println(90);
-  delay(20000);
+  delay(21000);
 
   // step 1.5
   Serial.flush();
   Serial.write(0x91);
   Serial.flush();
-  delay(3000);
+  delay(7000);
 
   // step 2
   Serial.flush();
   Serial.write(0x92);
   Serial.flush();
-  delay(18000);
+  delay(25000);
 
   // step 2.5
   Serial.flush();
   Serial.write(0x93);
   Serial.flush();
-  delay(8000);
+  delay(5000);
 
   // step3
   Serial.flush();
   Serial.write(0x94);
   Serial.flush();
-  delay(8000);
+  delay(17000);
 
   // step3.5
   Serial.flush();
   Serial.write(0x95);
   Serial.flush();
-  delay(7000);
+  delay(5000);
 
   // step 4
   Serial.flush();
   Serial.write(0x96);
   Serial.flush();
-  delay(4000);
+  delay(7000);
 
 }
